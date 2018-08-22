eeb4345b04f21f3d89160e738830e2ce3ee139ca
Nolen Ramminger <nolenramminger@gmail.com>
2018-08-22 00:39:40 -0600

    Coninued starter kit Pruning
    Removing additional unnecessary files from starter kit

--- src/components/containers/JamDicePage.js
+++ src/components/containers/JamDicePage.js
@@ -2,7 +2,6 @@ import React from 'react';
 //import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import * as actions from '../../actions/fuelSavingsActions';
 import DiceRollArea from '../DiceRollArea';
 
 export class JamDicePage extends React.Component {
